706005d79a75d0a6dd8754564cfd12748c32ef0d
bokigolic <bokigolic32@gmail.com>
2023-06-01 22:34:33 -0400

    Update form

--- src/rate Form/Rate.js
+++ src/rate Form/Rate.js
@@ -5,9 +5,17 @@ const Rate = () => {
   const [score, setScore] = useState('10');
   const [comment, setComment] = useState('');
   const [results, setResults] = useState([]);
+  const maxCharacters = 50;
 
   const handleChange = (e) => {
     setScore(e.target.value);
+  };
+
+  const handleTextAreaChange = (e) => {
+    const inputText = e.target.value;
+    if (inputText.length <= maxCharacters) {
+      setComment(inputText);
+    }
   };
 
   const handleSubmit = (e) => {
@@ -27,28 +35,25 @@ const Rate = () => {
     console.log('Score:', score);
   };
 
-  const handleChangeArea = (e) => {
-    setComment(e.target.value);
-  };
-
   return (
-    <div>
+    <div className="container">
       <form onSubmit={handleSubmit}>
         <fieldset>
           <h2>Feedback Form</h2>
           <div className="Field">
             <label>Score: {score} &#9733;</label>
-            <input
-              type="range"
-              min="0"
-              max="10"
-              value={score}
-              onChange={handleChange}
-            />
+            <input type="range" min="0" max="10" value={score} onChange={handleChange} />
           </div>
           <div>
             <label>Comment: </label>
-            <textarea onChange={handleChangeArea} value={comment}></textarea>
+            <div className="TextAreaContainer">
+              <textarea
+                onChange={handleTextAreaChange}
+                value={comment}
+                maxLength={maxCharacters}
+              ></textarea>
+              <div className="CharacterCount">{maxCharacters - comment.length}</div>
+            </div>
           </div>
           <button type="submit">Submit</button>
         </fieldset>
