0f3d0b29087b68935a682868b7fee507ae20a110
bokigolic <bokigolic32@gmail.com>
2023-06-27 13:11:03 -0400

    update feedback form

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Sound from './import Sound/Sound';
+
 import Rate from './rate Form/Rate';
 
 function App() {
   return (
     <div className="App">
       <Rate />
+   
       {/*<Sound />*/}
     </div>
   );
